418995ee0f5972aed3701d11e4bd4a0f44b9fdce
Luis <luisangeltrebejo@gmail.com>
2024-11-08 20:35:22 -0300

    Creacion de la clase Balatro

---
+++ src/main/java/edu/fiuba/algo3/Balatro.java
@@ -0,0 +1,33 @@
+package edu.fiuba.algo3;
+
+public class Balatro {
+    private int rondas;
+    private int puntajeActual;
+    private int rondaActual;
+
+    private Jugador jugador = new Jugador();
+    private Ronda ronda = new Ronda();
+    private Panel panel = new Panel();
+    private Tienda tienda = new Tienda();
+
+    public void juego()
+    {
+
+    }
+    public void iniciarJuego()
+    {
+
+    }
+    public void verificarPuntaje()
+    {
+
+    }
+    public void mostrarTienda()
+    {
+
+    }
+    private void iniciarRonda()
+    {
+
+    }
+}
