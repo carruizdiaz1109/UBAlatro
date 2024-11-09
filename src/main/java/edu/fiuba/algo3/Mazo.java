d0d402104f10a778da444b06eb4c935a3d275b17
Mariana <mpizzi@fi.uba.ar>
2024-11-08 23:09:09 -0300

    Creo clase Mazo con su constructor

---
+++ src/main/java/edu/fiuba/algo3/Mazo.java
@@ -0,0 +1,36 @@
+package edu.fiuba.algo3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+
+public class Mazo {
+    private List<CartaPoker> cartas;
+    private int cantidadCartas;
+
+    public Mazo(){
+        cartas = new ArrayList<>();
+        inicializarMazo();
+        mezclar();
+    }
+
+    protected void inicializarMazo(){
+        cartas.clear();
+        String[] palos = {"Pica", "Diamantes", "Corazones", "Treboles"};
+        int[] valores = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+        for (String palo : palos) {
+            for (int valor : valores) {
+                cartas.add(new CartaPoker(valor, palo));
+                cantidadCartas++;
+            }
+        }
+    }
+
+    public int getCantidadCartas() {
+        return cantidadCartas;
+    }
+
+    protected void mezclar() {
+        Collections.shuffle(cartas);
+    }
+}
