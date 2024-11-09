32ee1f84d5ca1f4cede3fb0447bfed01a9f6ceb8
Mariana <mpizzi@fi.uba.ar>
2024-11-08 23:09:49 -0300

    Creo Test de la clase mazo

---
+++ src/main/test/edu/fiuba/algo3/entrega_1/MazoTest.java
@@ -0,0 +1,27 @@
+package edu.fiuba.algo3.entrega_1;
+
+import edu.fiuba.algo3.ErrorMazoVacio;
+import edu.fiuba.algo3.Mazo;
+import edu.fiuba.algo3.CartaPoker;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MazoTest {
+    @Test
+    public void test01CantidadDeCartasEnMazoInicializado(){
+        Mazo mazo = new Mazo();
+
+        int cantidadCartasEsperadas = 52;
+        int cantidadCartasObtenidas = 0;
+
+        cantidadCartasObtenidas = mazo.getCantidadCartas();
+
+        assertEquals(cantidadCartasEsperadas, cantidadCartasObtenidas);
+    }
+}
