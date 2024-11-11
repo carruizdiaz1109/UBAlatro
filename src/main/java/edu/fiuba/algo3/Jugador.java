dd5e5f9223751cfa58112cf48ae75a93a8ca0676
carruizdiaz1109 <cruiz@fi.uba.ar>
2024-11-11 15:07:53 -0300

    Actualizacion clase jugador

--- src/main/java/edu/fiuba/algo3/Jugador.java
+++ src/main/java/edu/fiuba/algo3/Jugador.java
@@ -1,15 +1,32 @@
 package edu.fiuba.algo3;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Jugador {
     private String nombre;
+    private int puntaje;
+    private Mano manoActual;
+    private Mazo mazo;
 
-    public Jugador(String unNombre){
-        this.nombre = unNombre;
+    public Jugador(String nombre){
+        this.nombre = nombre;
+        this.puntaje = 0;
+        this.manoActual = new Mano(new ArrayList<>());
+        this.mazo = new Mazo();
     }
 
-    public void iniciarRonda(){}
+    public boolean esMismoPuntaje(int unPuntaje){
+        return this.puntaje == unPuntaje;
+    }
+
+    public boolean esMismoNombre(String unNombre){
+        return nombre.equals(unNombre);
+    }
+
+    public void iniciarRonda(){
+        manoActual = mazo.repartir();
+    }
 
     public Jugada jugar(){
         List<CartaPoker> cartas = List.of(new CartaPoker(1, Palo.PICAS)); // esto está hardcodeado
@@ -17,3 +34,6 @@ public class Jugador {
     };
 
 }
+
+
+
