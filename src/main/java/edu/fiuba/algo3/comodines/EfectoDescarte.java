972b417fadb34336dd784abe2b49131da77a38bc
carruizdiaz1109 <cruiz@fi.uba.ar>
2024-11-18 18:01:01 -0300

    Creo clase Descarte que hereda de Jugada y EfectoDescarte que hereda de Comodin

--- src/main/java/edu/fiuba/algo3/comodines/EfectoDescarte.java
+++ src/main/java/edu/fiuba/algo3/comodines/EfectoDescarte.java
@@ -1,4 +1,24 @@
 package edu.fiuba.algo3.comodines;
 
-public class EfectoDescarte {
+
+import edu.fiuba.algo3.Comodin;
+import edu.fiuba.algo3.Jugada;
+
+
+public class EfectoDescarte extends Comodin {
+
+    private final Class<? extends Jugada> descarte;
+
+    public EfectoDescarte(Class<? extends Jugada> unDescarte, int valor, int multiplicador) {
+        super(valor, multiplicador);
+        this.descarte = unDescarte;
+    }
+
+    @Override
+    public void aplicar(Jugada unDescarte) {
+        if (this.descarte.isInstance(unDescarte)) {
+            unDescarte.aplicarComodin(this.puntaje);
+        }
+    }
+
 }
