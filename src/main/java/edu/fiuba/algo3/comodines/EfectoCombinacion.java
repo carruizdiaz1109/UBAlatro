7bf2a2999d0e854ab52376fc791919d78ebfca19
carruizdiaz1109 <cruiz@fi.uba.ar>
2024-11-18 21:19:40 -0300

    Creo clase Efecto Combinado

--- src/main/java/edu/fiuba/algo3/comodines/EfectoCombinacion.java
+++ src/main/java/edu/fiuba/algo3/comodines/EfectoCombinacion.java
@@ -1,4 +1,22 @@
 package edu.fiuba.algo3.comodines;
 
-public class EfectoCombinacion {
+import edu.fiuba.algo3.Comodin;
+import edu.fiuba.algo3.Jugada;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EfectoCombinacion extends Comodin {
+    private final List<Comodin> combinaciones;
+
+    public EfectoCombinacion(List <Comodin> lasCombinacines, int valor, int multiplicador) {
+        super (valor, multiplicador);
+        this.combinaciones = lasCombinacines;
+    }
+
+
+    public void aplicar(Jugada unaJugada) {
+        for (Comodin comodin : this.combinaciones) {
+            //unaJugada.aplicarComodin(comodin.puntaje);
+        }
+    }
 }
