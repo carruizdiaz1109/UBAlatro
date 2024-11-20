9874122116ca8de58fb5f6411020471bb216e0e2
mateoSparta <msparta@fi.uba.ar>
2024-11-20 16:23:25 -0300

    hago que la pantalla emergente ocupe toda la pantalla

--- src/main/java/edu/fiuba/algo3/Main.java
+++ src/main/java/edu/fiuba/algo3/Main.java
@@ -1,40 +1,50 @@
 package edu.fiuba.algo3;
 
+import java.awt.*;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
 import java.util.List;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
+
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+import javafx.scene.image.Image;
 
 public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) {
-        // Crear botones
-        Button button1 = new Button("Botón 1");
-        Button button2 = new Button("Botón 2");
-
-        // Evento en botón
-        button1.setOnAction(e -> System.out.println("¡Botón 1 presionado!"));
-        button2.setOnAction(e -> System.out.println("¡Botón 2 presionado!"));
-
-        // Layout
-        VBox layout = new VBox(10); // Espaciado de 10px
-        layout.getChildren().addAll(button1, button2);
-
-        // Escena y ventana
-        Scene scene = new Scene(layout, 300, 200);
-        primaryStage.setTitle("Aplicación JavaFX");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
 
     public static void main(String[] args) {
         launch(args);
     }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        /*
+        // Layout
+        VBox layout = new VBox(10); // Espaciado de 10px
+        layout.getChildren().addAll(button1, button2);
+        */
+        // Escena y ventana
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.BLACK);
+
+        Image icon = new Image(Objects.requireNonNull(getClass().getResource("/icon.png")).toString());
+        stage.getIcons().add(icon);
+        stage.setFullScreen(true);
+        stage.setFullScreenExitHint("perdiste");
+        stage.setTitle("UBAlatro");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,6 @@ module edu.fiuba.algo3 {
     exports edu.fiuba.algo3.jugadas;
     exports edu.fiuba.algo3.comodines;
     requires com.fasterxml.jackson.databind;
-    requires java.xml;
+    requires java.desktop;
 
 }
