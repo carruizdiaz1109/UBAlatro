7c7692bd6afef0e8f2fd24e104caa0aa4a748c47
carruizdiaz1109 <cruiz@fi.uba.ar>
2024-11-21 18:55:05 -0300

    Leo el archivo json de comodines jugada y comodin puntaje

---
+++ src/main/test/edu/fiuba/algo3/entrega_3/TiendaTest.java
@@ -0,0 +1,15 @@
+package edu.fiuba.algo3.entrega_3;
+
+import edu.fiuba.algo3.Tienda;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TiendaTest {
+    @Test
+    public void test01SeInicializanCorrectamenteComodines() {
+        Tienda tienda = new Tienda();
+        int cantidad = tienda.obtenerComodines().size();
+        assertEquals(100,cantidad);
+    }
+}
