f5df1cc5e56a71c683397a3d32392cd44a2daee0
Mariana <mpizzi@fi.uba.ar>
2024-11-22 20:24:57 -0300

    test ronda

--- src/main/test/edu/fiuba/algo3/entrega_1/RondaTest.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/RondaTest.java
@@ -1,47 +1,88 @@
 package edu.fiuba.algo3.entrega_1;
 
-import edu.fiuba.algo3.Balatro;
+import edu.fiuba.algo3.*;
+import edu.fiuba.algo3.jugadas.Descarte;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import edu.fiuba.algo3.Jugador;
-import edu.fiuba.algo3.Jugada;
-import edu.fiuba.algo3.Ronda;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 public class RondaTest {
-    /*
-    Jugador jugadorMock;
-    Jugada jugadaMock;
-    Balatro balatroMock;
+
+    private Ronda ronda;
+    private Tienda tiendaMock;
+    private Jugada jugadaMock;
+    private Descarte descarteMock;
 
     @BeforeEach
     public void setup() {
-        jugadorMock = mock(Jugador.class);
+        tiendaMock = mock(Tienda.class);
         jugadaMock = mock(Jugada.class);
-        balatroMock = mock(Balatro.class);
+        descarteMock = mock(Descarte.class);
+
+        ronda = new Ronda(1, 3000, 3, 3, tiendaMock);
     }
 
     @Test
-    public void test01AlIniciarRondaSeLlamaAIniciarRondaJugador() {
-        Ronda ronda1 = new Ronda(jugadorMock);
-
-        when(jugadorMock.jugar()).thenReturn(jugadaMock);
-        when(jugadaMock.calcularValor()).thenReturn(600);
-
-        ronda1.iniciarRonda();
-        //verify(jugadorMock,times(1)).iniciarRonda();
+    public void test01VerificarPuntajeConPuntajeInsuficiente() {
+        when(jugadaMock.calcularPuntaje()).thenReturn(2500);
+        ronda.agregarJugada(jugadaMock);
+        assertFalse(ronda.verificarPuntaje());
     }
 
     @Test
-    public void test02SeVerificaElPuntajeDeLaRonda() {
-        Ronda ronda1 = new Ronda(jugadorMock);
-
-        when(jugadorMock.jugar()).thenReturn(jugadaMock);
-        when(jugadaMock.calcularValor()).thenReturn(600);
-
-        ronda1.iniciarRonda();
-        assertTrue(ronda1.verificarPuntaje());
+    void test02VerificarPuntajeConPuntajeSuficiente() {
+        // Simular que calcularTotalRonda devuelve 3500
+        when(jugadaMock.calcularPuntaje()).thenReturn(3000);
+        ronda.agregarJugada(jugadaMock);
+        assertTrue(ronda.verificarPuntaje());
     }
-*/
+
+    @Test
+    void test03AgregarJugadaExitosamente() {
+        when(jugadaMock.calcularPuntaje()).thenReturn(1000);
+
+        ronda.agregarJugada(jugadaMock);
+        int puntajeRonda = ronda.calcularTotalRonda();
+        assertEquals(1000, puntajeRonda);
+    }
+
+    @Test
+    void test04ErrorAgregarJugadaSinJugadasDisponibles() {
+        ronda.agregarJugada(jugadaMock);
+        ronda.agregarJugada(jugadaMock);
+        ronda.agregarJugada(jugadaMock);
+
+        assertThrows(ErrorNoHayJugadasDisponibles.class, () -> ronda.agregarJugada(jugadaMock));
+    }
+
+    @Test
+    void test05AgregarDescarteExitosamente(){
+        when(descarteMock.calcularPuntaje()).thenReturn(500);
+        ronda.agregarDescarte(descarteMock);
+
+        assertEquals(500, ronda.calcularTotalRonda());
+    }
+
+    @Test
+    void test06ErrorAgregarDescarteSinDescarteDisponibles() {
+        ronda.agregarDescarte(descarteMock);
+        ronda.agregarDescarte(descarteMock);
+        ronda.agregarDescarte(descarteMock);
+
+        assertThrows(ErrorNoHayDescarteDisponibles.class, () -> ronda.agregarDescarte(descarteMock));
+    }
+
+    @Test
+    void test07CalcularTotalRonda() {
+        when(jugadaMock.calcularPuntaje()).thenReturn(1000);
+        when(descarteMock.calcularPuntaje()).thenReturn(500);
+
+        ronda.agregarJugada(jugadaMock);
+        ronda.agregarDescarte(descarteMock);
+
+        assertEquals(1500, ronda.calcularTotalRonda());
+    }
+
 }
