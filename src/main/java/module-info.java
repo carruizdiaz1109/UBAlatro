356135990ec82fa46a8f4385ad15fde2fef7630b
carruizdiaz1109 <cruiz@fi.uba.ar>
2024-11-24 11:29:42 -0300

    Soluciono problema: se leen los test menor el de balatro.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,12 @@ module edu.fiuba.algo3 {
     requires javafx.controls;
     requires json.simple;
     exports edu.fiuba.algo3;
+    exports edu.fiuba.algo3.jugadas;
+    exports edu.fiuba.algo3.comodines;
     requires com.fasterxml.jackson.databind;
     requires java.desktop;
     requires javafx.fxml;
     requires javafx.graphics;
 
-    opens edu.fiuba.algo3 to javafx.fxml, org.junit.jupiter;
+    opens edu.fiuba.algo3 to javafx.fxml, org.junit.jupiter, to .org.mockito;
 }
