1e77c5ce92d51e86d0777c77bacdbb0780a87c16
Mariana <mpizzi@fi.uba.ar>
2024-11-26 12:05:50 -0300

    agrego test de ConjuntoCartas

---
+++ src/test/java/edu/fiuba/algo3/entrega_2/ConjuntoCartasTest.java
@@ -0,0 +1,54 @@
+package edu.fiuba.algo3.entrega_2;
+
+import edu.fiuba.algo3.CartaPoker;
+import edu.fiuba.algo3.ConjuntoCartas;
+import edu.fiuba.algo3.Palo;
+import edu.fiuba.algo3.Valor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+public class ConjuntoCartasTest {
+
+    @Test
+    public void test01SeAgregaUnaCarta(){
+        CartaPoker carta = new CartaPoker(Valor.DOS, Palo.PICAS);
+
+        List<CartaPoker> listaEsperada = new ArrayList<>();
+        listaEsperada.add(carta);
+        ConjuntoCartas conjuntoEsperado = new ConjuntoCartas(listaEsperada);
+
+        ConjuntoCartas conjunto = new ConjuntoCartas();
+        conjunto.agregarCarta(carta);
+
+        assert(conjunto.compararCon(conjuntoEsperado));
+    }
+
+    @Test
+    public void test02DevuelveTrueSiElConjuntoNoEstaVacio(){
+        CartaPoker carta = new CartaPoker(Valor.DOS, Palo.PICAS);
+
+        ConjuntoCartas conjunto = new ConjuntoCartas();
+        conjunto.agregarCarta(carta);
+
+        assertTrue(conjunto.tieneCartas());
+    }
+
+    @Test
+    public void test03EliminarCartasDevuelveConjuntoVacio(){
+        ConjuntoCartas conjuntoEsperado = new ConjuntoCartas();
+        CartaPoker carta = new CartaPoker(Valor.DOS, Palo.PICAS);
+
+        ConjuntoCartas conjunto = new ConjuntoCartas();
+        conjunto.agregarCarta(carta);
+
+        conjunto.eliminarCartas();
+
+        assert(conjuntoEsperado.compararCon(conjuntoEsperado));
+    }
+}
