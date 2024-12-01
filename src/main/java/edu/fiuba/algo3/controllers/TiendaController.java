e6869cd53661bc9c4fa499ce9bea59240898cc33
carruizdiaz1109 <cruiz@fi.uba.ar>
2024-12-01 03:49:24 -0300

    INtento de codigo para leer comodines

--- src/main/java/edu/fiuba/algo3/controllers/TiendaController.java
+++ src/main/java/edu/fiuba/algo3/controllers/TiendaController.java
@@ -1,14 +1,18 @@
 package edu.fiuba.algo3.controllers;
 
 import edu.fiuba.algo3.modelo.entidades.Jugador;
+import edu.fiuba.algo3.modelo.entidades.comodines.Comodin;
+import edu.fiuba.algo3.vistas.ComodinVisual;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.fxml.FXMLLoader;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class TiendaController {
     private Jugador jugador;
@@ -17,12 +21,28 @@ public class TiendaController {
     @FXML
     private Button btnContinuar;
 
+    @FXML
+    private StackPane comodines;
+
+
     public void setJugador(Jugador jugador) {
         this.jugador = jugador;
     }
 
     public void setStage(Stage stage) {
         this.stage = stage;
+    }
+
+    public void visualizarComodines() {
+        this.comodines.getChildren().clear();
+        ArrayList<Comodin> comodines = this.jugador.obtenerComodines();
+
+        for (Comodin comodin : comodines) {
+            String imagePath = "/imagenes/comodines/" + comodin.getNombre() + ".png";
+            ComodinVisual comodinVisual = new ComodinVisual(comodin, imagePath, 100, 150);
+
+            this.comodines.getChildren().add(comodinVisual);
+        }
     }
 
     @FXML
