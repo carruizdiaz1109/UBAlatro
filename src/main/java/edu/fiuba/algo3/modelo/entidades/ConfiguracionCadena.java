6d8fc3aca920ebe296f6d4f8a4ede7fa807239ff
caror <cruiz@fi.uba.ar>
2024-12-03 13:35:13 -0300

    INTENTO DE OBSERVER

---
+++ src/main/java/edu/fiuba/algo3/modelo/entidades/ConfiguracionCadena.java
@@ -0,0 +1,5 @@
+package edu.fiuba.algo3.modelo.entidades;
+
+public class ConfiguracionCadena {
+
+}
--- src/main/java/edu/fiuba/algo3/modelo/entidades/tarots/TarotJugada.java
+++ src/main/java/edu/fiuba/algo3/modelo/entidades/tarots/TarotJugada.java
@@ -3,33 +3,32 @@ package edu.fiuba.algo3.modelo.entidades.tarots;
 import edu.fiuba.algo3.modelo.excepciones.TarotDistintaJugadaError;
 import edu.fiuba.algo3.modelo.entidades.Jugada;
 import edu.fiuba.algo3.modelo.entidades.Puntaje;
+import edu.fiuba.algo3.modelo.entidades.ConfiguracionCadena;
+
+import java.lang.module.Configuration;
 
 public class TarotJugada extends Tarot {
 
+    private final String ejemplar;
+
     public TarotJugada(String nombre, String descripcion, Puntaje efecto, String ejemplar) {
         super(nombre, descripcion, efecto, "mano", ejemplar);
+        this.ejemplar = ejemplar;
     }
 
     @Override
     public void aplicar(Object objeto) {
         if (objeto instanceof Jugada) {
-            aplicar((Jugada) objeto);
+            aplicar();
         } else {
             throw new IllegalArgumentException("El objeto no es una Jugada");
         }
     }
 
-    public void aplicar(Jugada unaJugada) {
-        if(sePuedeUtilizar(unaJugada)){
-            unaJugada.aplicarTarot(this.puntaje);
-        }else{
-            throw new TarotDistintaJugadaError();
-        }
+    public void aplicar() {
+        ConfiguracionCadena.setPuntaje(ejemplar, efecto);
     }
 
-    private boolean sePuedeUtilizar(Jugada unaJugada){
-        return this.ejemplar.equalsIgnoreCase(unaJugada.getClass().getSimpleName());
-    }
 }
 
 
