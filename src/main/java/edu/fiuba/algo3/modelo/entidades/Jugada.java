e12e584bcc329c58b7d4cd595bfc0c9141a7a5bf
mateoSparta <msparta@fi.uba.ar>
2024-12-04 13:04:42 -0300

    arrelgo la manera de calcular puntaje descarte (esto ya estaba hecho en alguna rama)

--- src/main/java/edu/fiuba/algo3/modelo/entidades/Jugada.java
+++ src/main/java/edu/fiuba/algo3/modelo/entidades/Jugada.java
@@ -97,9 +97,5 @@ public abstract class Jugada implements Evaluable {
         this.puntajeComodin = this.puntajeComodin.sumarPuntaje(unPuntaje);
     }
 
-    public void aplicarTarot(Puntaje unPuntaje){
-        this.puntaje = unPuntaje;
-    }
-
     public Puntaje obtenerPuntaje() { return this.puntaje; }
 }
